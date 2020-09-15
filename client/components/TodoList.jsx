accdae8ab470cb97ddc889f71593a2a9a72d3876
Shrena Patel <shrena.p@hotmail.co.uk>
2020-09-16 11:45:16 +1200

    pseudocode city

--- client/components/TodoList.jsx
+++ client/components/TodoList.jsx
@@ -1,42 +1,60 @@
 import React from 'react'
 import AddTodo from './AddTodo'
-import EditTodo from './EditTodo'
+
 import { connect } from 'react-redux'
 import { initTask, deleteTask, updateTask } from '../actions/tasks'
-
 import { getAllTasks, apiDeleteTask, apiUpdateTask } from '../apis/tasks'
-
 
 
 class TodoList extends React.Component {
 
+    // SET INITIAL STATE ===================================================
+    // if using numbers, set initial state to null. Below, the editTask property will take the id of task, which is a number
+    // For the filter property, we're setting it to 'all' as the default, so that 'All' in the footer of the todo list has a box around it by default
+
     state = {
-        editTask: null,   // if using numbers(id), set initial state to null
-        filter: 'all' // this needs to be set to active or completed when clicked on. when checked = true - completed, when checked = false - active
+        editTask: null,   
+        filter: 'all'
     }
 
-
+    // ON COMPONENT LOAD ====================================================
+    // 1. call the getAllTasks API function 
+    // 2. dispatch the initTask function to redux, taking 'task' as the function's parameter ('task' is the result of the getAllTasks API function)
+    
     componentDidMount() {
         getAllTasks()
             .then(task => (this.props.dispatch(initTask(task))))
     }
 
+    // PRESS ENTER TO UPDATE TASK ============================================
+    // If the key that is pressed down is 'Enter',
+    // 1. make a new variable called new task, that is set to equal an object.
+    // 2. this object sets the task property to take what is written in the update task input field.
+    // 3. call the apiUpdateTask function, taking in a taskId and the newTask variable
+    // 4. updatedTask is what is returned from the apiUpdateTask function 
+    // 5. disptach the updateTask function to redux, taking the taskId and newTask as parameters.
+
     handleKeyPress = (taskId, event) => {
         if (event.key == 'Enter') {
-            // return apiUpdateTask(id, task)
+            
             console.log(event.target.value)
             const newTask = { task: event.target.value }
 
             apiUpdateTask(taskId, newTask)
                 .then(updatedTask => {
-                    console.log(updatedTask)
-                    this.props.dispatch(updateTask(taskId, newTask)) //need to change
-                    //next: change what todos are being returned when you map over them - for clicking on completed tick box
-                    //if task.completed is true, then show in the completed section
-
+                    this.props.dispatch(updateTask(taskId, newTask))
                 })
         }
     }
+
+    // ON CLICK, CHANGE 'COMPLETED' PROPERTY ====================================
+    // When you click on the green tick input button, change the 'completed' property of task to be true.
+    // When you click again to get rid of the tick, chnage the 'completed' property of task to be false.
+    // 1. define a completed variable that is equal to an object
+    // 2. this object sets a completed property to whether or not the tick input is checked
+    // 3. then set the actual 'completed' property of task to equal event.target.checked
+    // 4. call the apiUpdateTask function, taking the task.id and completed as the parameters
+    // 5. dispatch the updateTask function to redux, taking task.id and task as the parameters
 
     handleOnClick = (task, event) => {
         // console.log(event.target.checked)
@@ -51,13 +69,16 @@ class TodoList extends React.Component {
             })
     }
 
+    // ON CLICK, SET STATE FOR FILTERING ACTIVE/COMPLETED TASKS =============================
+    // handleFilterClick takes event and filter as parameters
+    // 1. set state to have a filter property, that takes the filter passed into handleFilterClick as the value of the property
+    // 2. this is then used in the filter function below.
+
     handleFilterClick = (event, filter) => {
         event.preventDefault()
-        console.log(filter)
         this.setState({
             filter: filter
         })
-        // ( task.completed == true )  ? this.state.filter == 'completed' : this.state.filter == 'active'
     }
 
     render() {
@@ -67,13 +88,17 @@ class TodoList extends React.Component {
 
                 <section className="main">
                     <input id="toggle-all" className="toggle-all" type="checkbox" />
-                    {/* onClick={(showtasks == true) ? showtasks : hidetasks} */}
                     {/* ABOVE input is the down arrow next to 'What needs to be done?' */}
                     <label htmlFor="toggle-all">Mark all as complete</label>
                     <ul className="todo-list">
 
+                        {/* Filter thorugh task - if 'active' set task.completed to false. if 'completed', set task.completed to true. Then do the map function afterwards. */}
+                        {/* this.state.filter refers to the state property of 'filter' */}
+                       
+                        {/* FILTER FUNCTION ==================================
+                        This function */}
                         {this.props.tasks.filter(task => {
-                            switch(this.state.filter) {
+                            switch (this.state.filter) {
                                 case 'active':
                                     return task.completed == false
                                 case 'completed':
